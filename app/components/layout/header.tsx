c99c760fb5894227998d12d800b74cc5552df645
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-06-04 15:49:24 +0900

    feat: add alert modal

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Sunny from "../../../public/assets/Icon/sunny.svg";
+import AlertModal from "../Modal/alertModal";
 
 type HeaderProps = {
   username: string;
@@ -12,10 +13,27 @@ type HeaderProps = {
 
 const Header = ({ username, onLogout, onToggleSidebar }: HeaderProps) => {
   const [rotate, setRotate] = useState(false);
+  const [openAlert, setOpenAlert] = useState(false);
+  const notifications = [
+    {
+      username: "admin",
+      message: "신고가 접수되었습니다.",
+    },
+    { username: "admin", message: "지난 주 대비 10%의 사용자가 증가했습니다." },
+    {
+      username: "admin",
+      message: "SUNNY에 대한 피드백이 접수 되었습니다.",
+    },
+  ];
 
   const handleClick = () => {
     setRotate(true);
     setTimeout(() => setRotate(false), 1000); // 1초 후에 애니메이션 클래스 제거
+  };
+
+  const handleAlertBtnClick = () => {
+    setOpenAlert(!openAlert);
+    console.log("알림 버튼 누름 ::: ", openAlert);
   };
 
   return (
@@ -37,7 +55,7 @@ const Header = ({ username, onLogout, onToggleSidebar }: HeaderProps) => {
         </button>
         <div className="text-lg font-bold">Welcome, {username}!</div>
       </div>
-      <div className="flex items-center space-x-2">
+      <div className="relative flex items-center space-x-2">
         <button
           onClick={() => alert("프로필 버튼 클릭")}
           className="px-2 py-1 font-bold text-black border rounded sm:px-4 sm:py-2 border-orange_4 bg-basic_1 hover:border-orange_5 hover:bg-basic_2 hover:text-gray_6"
@@ -45,11 +63,19 @@ const Header = ({ username, onLogout, onToggleSidebar }: HeaderProps) => {
           프로필
         </button>
         <button
-          onClick={() => alert("알림 버튼 클릭")}
+          onClick={handleAlertBtnClick}
           className="px-2 py-1 font-bold text-black border rounded sm:px-4 sm:py-2 border-orange_4 bg-basic_1 hover:border-orange_5 hover:bg-basic_2 hover:text-gray_6"
         >
           알림
         </button>
+        {openAlert && (
+          <div className="absolute right-0 mt-2 top-full">
+            <AlertModal
+              notifications={notifications}
+              onClose={() => setOpenAlert(false)}
+            />
+          </div>
+        )}
         <button
           onClick={onLogout}
           className="px-2 py-1 font-bold text-black border rounded sm:px-4 sm:py-2 border-orange_4 bg-basic_1 hover:border-orange_5 hover:bg-basic_2 hover:text-gray_6"
