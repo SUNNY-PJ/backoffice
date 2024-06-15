6367b0232f16683e9aef9b5ba9b3bb86fe9fbe30
yeoniii20 <commetoi_yeoni@naver.com>
2024-06-15 16:39:30 +0900

    feat: add card compoent

--- app/(dashboard)/dashboard/recent/page.tsx
+++ app/(dashboard)/dashboard/recent/page.tsx
@@ -1,7 +1,24 @@
+import Card from "@/app/components/card/card";
+import { FaExclamationTriangle, FaCheckCircle } from "react-icons/fa";
+
 const Recent = () => {
   return (
     <>
-      <div className="flex flex-col h-screen text-black">Recent</div>
+      <div className="text-black p-4">log</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <Card
+          title="Errors"
+          count={5}
+          icon={<FaExclamationTriangle size={24} />}
+          color="bg-red-500"
+        />
+        <Card
+          title="Success"
+          count={120}
+          icon={<FaCheckCircle size={24} />}
+          color="bg-green-500"
+        />
+      </div>
     </>
   );
 };
