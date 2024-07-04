412c7c0e5c53486d10fc157a7714ad9c51264b3d
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-04 11:21:35 +0900

    feat: add mock.js

---
+++ data/mock.js
@@ -0,0 +1,11 @@
+export const NOTIFICATION = [
+  {
+    username: "admin",
+    message: "신고가 접수되었습니다.",
+  },
+  { username: "admin", message: "지난 주 대비 10%의 사용자가 증가했습니다." },
+  {
+    username: "admin",
+    message: "SUNNY에 대한 피드백이 접수 되었습니다.",
+  },
+];
