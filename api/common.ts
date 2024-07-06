aa55e95875cf1c62a14d02046d9167e2244b706a
yeoniii20 <commetoi_yeoni@naver.com>
2024-07-06 11:50:49 +0900

    fix: change redirect uri

--- api/common.ts
+++ api/common.ts
@@ -6,5 +6,6 @@ export const stompUrl = "http://localhost:8080/stomp";
 
 // kakao
 export const REST_API_KEY = "7ff971db2010c97a3e191dd319ec45cd";
-export const REDIRECT_URI = "http://43.201.176.22:8080";
+export const REDIRECT_URI = "http://localhost:3000/auth/kakao/callback";
+// export const REDIRECT_URI = "http://43.201.176.22:8080/auth/kakao/callback";
 export const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
