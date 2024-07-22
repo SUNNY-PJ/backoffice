b3033166e17370f4fb0e714954359cced3738c87
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-07-22 17:14:13 +0900

    feat: add sdk script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,18 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Layout from "./components/layout/layout";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "SUNNY",
   description: "SUNNY BackOffice",
 };
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 export default function RootLayout({
   children,
@@ -17,10 +24,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/assets/Icon/favicon.svg" />
-        <body>
-          <Layout>{children}</Layout>
-        </body>
       </head>
+      <body>
+        <Script
+          async
+          src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.2/kakao.min.js"
+          integrity="sha384-TiCUE00h649CAMonG018J2ujOgDKW/kVWlChEuu4jK2vxfAAD0eZxzCKakxg55G4"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+        <Layout>{children}</Layout>
+      </body>
     </html>
   );
 }
