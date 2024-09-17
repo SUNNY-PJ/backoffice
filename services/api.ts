6a22c5bfe8d2a607bbb2204b8604fefa10cde4a7
yeoniii20 <commetoi_yeoni@naver.com>
2024-09-17 23:56:21 +0900

    feat: create get profile data

--- services/api.ts
+++ services/api.ts
@@ -15,3 +15,18 @@ export const getChatList = async (token: string) => {
     throw error; // 에러 발생 시 호출한 곳에서 처리할 수 있도록 에러를 던짐
   }
 };
+
+export const getUserProfile = async (token: string) => {
+  try {
+    const response = await axios.get("/api/user/profile", {
+      headers: {
+        Authorization: `Bearer ${token}`, // 토큰을 Authorization 헤더에 추가
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching chat list:", error);
+    throw error;
+  }
+};
