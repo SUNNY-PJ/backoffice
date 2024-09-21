8c483403d836b05cbcd4020e443c4513c0056ec6
yeoniii20 <commetoi_yeoni@naver.com>
2024-09-22 02:30:28 +0900

    fix: edit next.config

--- next.config.mjs
+++ next.config.mjs
@@ -29,34 +29,34 @@
 
 // /** @type {import('next').NextConfig} */
 
-// const nextConfig = {
-//   reactStrictMode: false,
-//   swcMinify: true,
-//   compiler: {
-//     styledComponents: true,
-//   },
-//   output: "standalone",
-//   // webpack5: true,
-//   webpack: (config) => {
-//     config.resolve.fallback = { fs: false };
-//     return config;
-//   },
-//   // experimental: {
-//   //   nextScriptWorkers: true,
-//   // },
-//   async rewrites() {
-//     return [
-//       {
-//         source: "/:path*",
-//         destination: `${process.env.NEXT_PUBLIC_DEV_BASE_URL}/:path*`,
-//       },
-//     ];
-//   },
-// };
-
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+  output: "standalone",
+  // webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+  // experimental: {
+  //   nextScriptWorkers: true,
+  // },
+  //   async rewrites() {
+  //     return [
+  //       {
+  //         source: "/:path*",
+  //         destination: `${process.env.NEXT_PUBLIC_DEV_BASE_URL}/:path*`,
+  //       },
+  //     ];
+  //   },
+};
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
