4d248aa80b255284122d75455b78ca31db70759c
yeoniii20 <commetoi_yeoni@naver.com>
2024-09-22 00:39:53 +0900

    feat: add getChatRoom

--- services/api.ts
+++ services/api.ts
@@ -30,3 +30,26 @@ export const getUserProfile = async (token: string) => {
     throw error;
   }
 };
+
+export const getChatRoom = async (
+  token: string,
+  chatRoomId: string,
+  size: number,
+  chatMessageId: any
+) => {
+  try {
+    const response = await axios.get(
+      `/api/chat/chatRoom?chatRoomId=${chatRoomId}&size=${size}&chatMessageId=${chatMessageId}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json; charset=utf-8",
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching chat list:", error);
+    throw error;
+  }
+};
